2d594f45f2fa23ff47c890c04f5537e2f04d0916
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-09 22:30:05 -0300

    iniciando com variáveis

---
+++ ruby/01_variáveis/01_declarando_variável.rb
@@ -0,0 +1,16 @@
+# Perguntar o nome do usuário
+
+# saída padrão
+print 'Digite seu nome: '
+
+# entrada de dados
+nome = gets.chomp
+
+print 'Digite seu sobrenome: '
+
+sobrenome = gets.chomp
+
+# utilizando puts para saída padrão
+puts "Olá, #{nome} #{sobrenome}! "
+
+ 
