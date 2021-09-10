e13965c794b287db2f18d512df976fc5e417c324
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-09 22:30:49 -0300

    tipos primitivos

---
+++ ruby/01_variáveis/03_tipos_primitivos.rb
@@ -0,0 +1,22 @@
+# tipos primitivos
+
+tipo_inteiro = 10
+puts tipo_inteiro
+puts tipo_inteiro.class
+puts()
+
+tipo_float = 400.9
+puts tipo_float
+puts tipo_float.class 
+puts()
+
+tipo_boo = true
+puts tipo_boo
+puts tipo_boo.class
+puts()
+
+
+tipo_string = 'Daniel'
+puts tipo_string
+puts tipo_string.class
+puts()
