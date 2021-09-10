26ce0ce91a3fa7aab4dbe2f7e4219c4d0518bc5d
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-10 11:19:54 -0300

    verificando a condição else

---
+++ ruby/02_condicionais/02_verificar_condição_else.rb
@@ -0,0 +1,11 @@
+# testar condição falsa e verdadeira
+
+dia = 'Sabado'
+
+if dia != 'Sabado'
+    estudar = 'Ruby'
+else
+    estudar = 'Rails'
+end
+
+puts "Hoje é dia de estudar sobre #{estudar}"
