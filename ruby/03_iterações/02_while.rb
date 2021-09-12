10dc2d2ccf2ef7a6646617adc6838bba6a0b8d3c
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-12 09:44:01 -0300

    enquanto

---
+++ ruby/03_iterações/02_while.rb
@@ -0,0 +1,11 @@
+# instrução que repete um bloco de código enquanto sua condição for verdadeira
+# enquanto todas as condições forem verdadeiras ela fica no loop e irá parar quando a condição for falsa.
+
+# contador de 1 até 10
+contador = 1
+
+while contador <= 10
+    puts contador
+
+    contador += 1  # enquanto o contador for menor que 10, ele adiciona um número.
+end
