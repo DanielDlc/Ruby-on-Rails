b8f6e7f4b646e2484fb31886cb3e17cc5ca8db89
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-12 09:44:11 -0300

    loop

---
+++ ruby/03_iterações/03_loop_do.rb
@@ -0,0 +1,11 @@
+# criando um loop até a condição for satisfeita e quebrar o loop com break
+
+contador = 1
+
+loop do
+    puts contador
+    if contador == 10
+        break
+    end
+    contador += 1
+end
