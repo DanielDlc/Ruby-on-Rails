6582218657b670828ff78044642d8bfe9914a92b
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-14 06:52:38 -0300

    each hash

---
+++ ruby/04_collections/02_each_hash.rb
@@ -0,0 +1,10 @@
+aulas = {
+    'Aula de terça:' => 'Linguagens formais e autômatos',
+    'Aula de quarta:' => 'Eletrecidade aplicada',
+    'Aula de quinta:' => 'Estrutura de dados',
+    'Aula de sexta:' => 'Sistemas Digitais'
+}
+
+aulas.each do |key, value|
+    puts "#{key} #{value}"
+end
