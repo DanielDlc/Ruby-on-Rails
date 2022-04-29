744f721d9e37a607ce37cd8d042541f0726dc740
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-29 20:45:31 -0300

    puts, gets chomp

---
+++ ruby/01_variáveis/06_gets.rb
@@ -0,0 +1,10 @@
+puts "what is your first name?"
+first_name = gets.chomp
+puts "what is your last_name?"
+last_name = gets.chomp
+
+full_name = "#{first_name} #{last_name}"
+puts "Your full name is #{full_name}"
+puts "your full name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.length} characters in it"
+
