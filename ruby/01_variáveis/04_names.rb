6d29071b17168d7720d711df2c804354f3f6b15c
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-29 14:30:55 -0300

    interpolação de string

---
+++ ruby/01_variáveis/04_names.rb
@@ -0,0 +1,9 @@
+# interpolação de string full_name
+
+first_name = "Daniel"
+last_name = "Costa"
+full_name = "#{first_name} #{last_name}"
+
+puts first_name + " " + last_name
+puts "My first name is #{first_name} and my last name is #{last_name}"
+puts full_name
