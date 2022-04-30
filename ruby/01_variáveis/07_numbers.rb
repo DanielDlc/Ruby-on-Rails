1bc16fcd74aace301c3c2c1382496df4be76bca3
DanielDlc <dlc.engcomputacao@gmail.com>
2022-04-30 12:34:26 -0300

    calculator number

---
+++ ruby/01_variáveis/07_numbers.rb
@@ -0,0 +1,12 @@
+puts "Simple calculator"
+20.times { print "-"}
+puts
+
+puts "Enter the first number"
+num_01 = gets.chomp
+
+puts "Enter the second number"
+num_02 = gets.chomp
+
+calc = num_01.to_i * num_02.to_i
+puts "The first number multiplied by the second number is #{calc}"
