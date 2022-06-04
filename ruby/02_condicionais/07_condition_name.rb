847a91ed61ead97fd69c0c434e6a65dcb541ad42
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-04 17:01:17 -0300

    condition name

---
+++ ruby/02_condicionais/07_condition_name.rb
@@ -0,0 +1,10 @@
+name = "Daniel"
+if name == "Daniel"
+    puts "Welcome to the program, Daniel"
+elsif name == "Diego"
+    puts "Welcome to the program, Diego"
+elsif name == "Jean"
+    puts "Go back to helping students Jean"
+else
+    puts "Welcome to the program, User"
+end
