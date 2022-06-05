e4ed912f6e985233ceb3d47e64aedf18e37acd08
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-05 09:49:00 -0300

    modificando arrays

---
+++ ruby/04_collections/04_modificandoArrays.rb
@@ -0,0 +1,10 @@
+numero = (1..10).to_a
+p numero.join  # tira todos os elementos do array e transforma em string
+
+p numero.join("-")  # colocando traços
+
+name = ["my", "name", "is", "Daniel"]
+puts name[3]
+
+outroNumero = (1..100).to_a
+p outroNumero.join(", ")  # colocar virgula e espaço
