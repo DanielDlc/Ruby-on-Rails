417ce8d5cf36a36036cd4efe6fe9fd6a3b22800f
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-11 19:02:14 -0300

    class estudantes

--- ruby/05_poo/student.rb
+++ ruby/05_poo/student.rb
@@ -1,25 +1,25 @@
 class Student
-    attr_accessor :first_name, :last_name, :email, :username
+    attr_accessor :first_name, :last_name, :username, :email
 
-    @first_name
-    @last_name
-    @email
-    @username
-
-    
-    def to_s
-        "First name: #{@first_name}"          
+    def initialize(first_name, last_name, username, email)
+        @first_name = first_name
+        @last_name = last_name
+        @username = username
+        @email = email
     end
+
+    def to_s
+        "First name: #{@first_name}, last name: #{last_name}, User name: #{username}, Email: #{email}"   
+    end
+
 end
 
-name = Student.new
-name.first_name = "Daniel"
-name.last_name = "Dlc"
-name.email = "dlc.engcomputacao@gmail.com"
-name.username = "Danfoyle"
+daniel = Student.new("Daniel", "Dlc", "Danfoyle", "dlc.engcomputacao@gmail.com")
+bianca = Student.new("Bianca", "Dias", "Bibia", "bia@mail.com")
 
+puts daniel
+puts bianca
 
-puts name.first_name = "Daniel"
-puts name.last_name = "Dlc"
-puts name.email = "dlc.engcomputacao@gmail.com"
-puts name.username = "Danfoyle"
+daniel.last_name = bianca.last_name
+puts "Daniel is altered"
+puts daniel
