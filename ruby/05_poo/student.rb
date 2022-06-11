b2ef76034d0718baf9a1212cb1bad94686798af9
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-11 15:28:01 -0300

    estrutura padrão de uma classe

--- ruby/05_poo/student.rb
+++ ruby/05_poo/student.rb
@@ -1,3 +1,14 @@
 class Student
-    
+    @first_name
+    @last_name
+    @email
+    @username
+    @pasword
+
+    def to_s
+        "First name: #{@first_name}"          
+    end
 end
+
+daniel = Student.new
+puts daniel
