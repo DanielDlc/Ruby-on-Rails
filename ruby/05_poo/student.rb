6d371420ae68f3bd313ca99aaeb6be420a4e8ad7
DanielDlc <dlc.engcomputacao@gmail.com>
2022-06-11 15:58:00 -0300

    class Student

--- ruby/05_poo/student.rb
+++ ruby/05_poo/student.rb
@@ -1,14 +1,25 @@
 class Student
+    attr_accessor :first_name, :last_name, :email, :username
+
     @first_name
     @last_name
     @email
     @username
-    @pasword
 
+    
     def to_s
         "First name: #{@first_name}"          
     end
 end
 
-daniel = Student.new
-puts daniel
+name = Student.new
+name.first_name = "Daniel"
+name.last_name = "Dlc"
+name.email = "dlc.engcomputacao@gmail.com"
+name.username = "Danfoyle"
+
+
+puts name.first_name = "Daniel"
+puts name.last_name = "Dlc"
+puts name.email = "dlc.engcomputacao@gmail.com"
+puts name.username = "Danfoyle"
